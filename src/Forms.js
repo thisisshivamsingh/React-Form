06f8c1f45e3d18f3614365606e1ce4488cd47130
Shivam <thisisshivam06@gmail.com>
2022-09-15 15:29:16 +0530

    Forms with multiple states

--- src/Forms.js
+++ src/Forms.js
@@ -1,15 +1,54 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 const Forms = () => {
-  const [h, setH] = useState("Default");
-  const getData = (e) => {
-    setH(e.target.value);
-    console.log(e.target.value);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [data, setData] = useState("data");
+
+  const firstNameChangeHandler = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const lastNameChangeHandler = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const mobileChangeHandler = (e) => {
+    setMobile(e.target.value);
+  };
+
+  const showData = (e) => {
+    e.preventDefault();
+    setData(firstName + "," + lastName + "," + mobile);
   };
   return (
     <div>
-      <h1>{h}</h1>
-      <input onChange={getData} type="text" />
+      <div>
+        <form onSubmit={showData}>
+          <input
+            type="text"
+            placeholder="First Name"
+            onChange={firstNameChangeHandler}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Last Name"
+            onChange={lastNameChangeHandler}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Mobile"
+            onChange={mobileChangeHandler}
+          />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+        {data}
+      </div>
     </div>
   );
 };
